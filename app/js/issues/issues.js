6fb2ccdd7d6d3c6fa4af72d1b6eef09d45dd702d
Konstantin Yanchev <yanchevkonstantin@gmail.com>
2016-05-07 19:57:13 +0300

    Added issues service.

---
+++ app/js/issues/issues.js
@@ -0,0 +1,120 @@
+"use strict";
+
+angular.module('IssueTrackingSystem.Issues.Issues', [])
+    .factory('issues', [
+        '$http',
+        '$q',
+        'notifier',
+        'BASE_URL',
+        function ($http, $q, notifier, BASE_URL) {
+            function getMyIssues(pageSize, pageNumber, orderBy) {
+                var pageSize = pageSize || 5,
+                    pageNumber = pageNumber || 1,
+                    orderBy = orderBy || 'DueDate',
+                    deferred = $q.defer();
+
+                $http.get(BASE_URL +
+                    'issues/me?pageSize=' +
+                    pageSize +
+                    '&pageNumber=' +
+                    pageNumber +
+                    '&orderBy=' +
+                    orderBy)
+                    .then(function (response) {
+                        deferred.resolve(response.data.Issues);
+                    }, function (error) {
+                        deferred.reject(error);
+                    });
+
+                return deferred.promise;
+            }
+
+            function addIssue(issue) {
+                var deferred = $q.defer();
+
+                $http.post(BASE_URL + 'issues', issue)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    }, function (error) {
+                        deferred.reject(error.data.error_description);
+                    });
+
+                return deferred.promise;
+            }
+
+            function editIssue(id, issue) {
+                var deferred = $q.defer();
+
+                $http.put(BASE_URL + 'issues/' + id, issue)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    }, function (error) {
+                        deferred.reject(error.data.error_description);
+                    });
+
+                return deferred.promise;
+            }
+
+            function changeStatus(id, statusId) {
+                var deferred = $q.defer();
+
+                $http.put(BASE_URL + 'issues/' + id + '/changestatus?statusid=' + statusId)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    }, function (error) {
+                        deferred.reject(error.data.error_description);
+                    });
+
+                return deferred.promise;
+            }
+            
+            function getIssueById(id) {
+                var deferred = $q.defer();
+
+                $http.get(BASE_URL + 'issues/' + id)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    },function (error) {
+                        deferred.reject(error.data.error_description);
+                    });
+
+                return deferred.promise;
+            }
+            
+            function getIssueComments(id) {
+                var deferred = $q.defer();
+
+                $http.get(BASE_URL + 'issues/' + id + '/comments')
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    },function (error) {
+                        deferred.reject(error.data.error_description);
+                    });
+
+                return deferred.promise;
+            }
+            
+            function addCommentToIssue(id, comment) {
+                var deferred = $q.defer();
+
+                $http.post(BASE_URL + 'issues/' + id + '/comments', comment)
+                    .then(function (response) {
+                        deferred.resolve(response.data);
+                    },function (error) {
+                        deferred.reject(error.data.error_description);
+                    });
+
+                return deferred.promise;
+            }
+
+            return {
+                getMyIssues: getMyIssues,
+                addIssue: addIssue,
+                editIssue: editIssue,
+                changeStatus: changeStatus,
+                getIssueById: getIssueById,
+                getIssueComments: getIssueComments,
+                addCommentToIssue: addCommentToIssue
+            };
+        }
+    ]);
